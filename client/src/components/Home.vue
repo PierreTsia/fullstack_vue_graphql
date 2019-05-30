b0992037411cbba0fb2d1de2d595ff3492233797
Pierre_T <pierre.tsiakkaros@gmail.com>
2019-05-30 18:52:23 +0200

    :building_with_construction: smart query inside Home.vue component :fire:

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,22 +1,15 @@
 <template>
-  <v-container>
-     <h1>Home</h1>
-     <ApolloQuery :query="getPostsQuery">
-       <template slot-scope="{ result: { data, loading, error  } }">
-        <div v-if="loading"> loading... </div>
-        <div v-if="error">
-           Oups! Something got wrong
-          <pre>{{ error }}</pre>
-        </div>
-         <ul v-if="data">
-           <li v-for="post in data.getPosts" :key="post._id">
-             {{ post.title }} | {{post.imageUrl}}
-           </li>
+  <v-container text-xs-center v-if="getPosts">
+    <v-flex xs12>
+      <v-carousel
+         v-bind="{ 'cycle': true }"
+         interval="3000">
+         <v-carousel-item v-for="post in getPosts" :key="post._id" :src="post.imageUrl" :value="post">
+            <h1 class="carousel_title">{{ post.title }}</h1>
+         </v-carousel-item>
+      </v-carousel>
+    </v-flex>
 
-         </ul>
-       </template>
-
-     </ApolloQuery>
   </v-container>
 </template>
 
@@ -27,19 +20,42 @@
     name: "home",
     data(){
       return {
-        getPostsQuery: gql `
-          query {
-            getPosts {
-              _id
-              title
-              imageUrl
-              description
-              likes
-            }
-          }
-        `
+        posts: []
       }
     },
-
+    apollo: {
+      getPosts: {
+        query: gql`
+        query {
+          getPosts {
+            _id
+            title
+            imageUrl
+            description
+            likes
+          }
+        }
+      `,
+        result({ data, loading }) {
+          if(!loading) {
+            this.posts = data.getPosts
+          }
+        },
+        error(err){
+          console.error('[ERROR!!]', err)
+          console.dir(err)
+        }
+      }
+    },
   };
 </script>
+<style lang="stylus">
+  .carousel_title
+    width 100%
+    position absolute
+    bottom 50px
+    color white
+    background-color rgba(0 0 0 0.5)
+    border-radius 5px 5px 0 0
+    padding 0.5em
+</style>
